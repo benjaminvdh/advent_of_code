501aa9234e106c1f259002b021da71ee77809dee
benjaminvdh <4235118+benjaminvdh@users.noreply.github.com>
2025-12-03 19:28:55 +0100

    Day 2 part 2

--- 2025/src/day_2.zig
+++ 2025/src/day_2.zig
@@ -15,8 +15,9 @@ pub fn main() !void {
 
     const ranges = try parse(lines.items[0], alloc);
 
-    const part_1 = countInvalid(ranges.items);
-    try aoc_2025.printPart1(part_1);
+    const part_1 = countInvalid(false, ranges.items);
+    const part_2 = countInvalid(true, ranges.items);
+    try aoc_2025.printDay(part_1, part_2);
 }
 
 fn parse(line: []const u8, alloc: std.mem.Allocator) !std.array_list.Aligned(Range, null) {
@@ -59,16 +60,13 @@ test parse {
     try std.testing.expectEqualDeep(&ref, parsed.items);
 }
 
-fn countInvalid(ranges: []const Range) usize {
+fn countInvalid(comptime at_least_two: bool, ranges: []const Range) usize {
     var sum_invalid: usize = 0;
 
     for (ranges) |range| {
         for (range.from..range.to + 1) |number| {
-            if (isInvalid(number)) {
-                //std.debug.print("{d} is invalid\n", .{number});
+            if (isInvalid(at_least_two, number)) {
                 sum_invalid += number;
-            } else {
-                //std.debug.print("{d} is valid\n", .{number});
             }
         }
     }
@@ -76,26 +74,7 @@ fn countInvalid(ranges: []const Range) usize {
     return sum_invalid;
 }
 
-fn isInvalid(number: usize) bool {
-    const l = std.math.log10_int(number);
-
-    for (1..l + 1) |i| {
-        const denominator = std.math.pow(usize, 10, i);
-        const remainder = std.math.rem(usize, number, denominator) catch 0;
-
-        if (remainder != 0 and std.math.log10_int(remainder) == i - 1) {
-            const reproduced = remainder + denominator * remainder;
-
-            if (reproduced == number) {
-                return true;
-            }
-        }
-    }
-
-    return false;
-}
-
-fn isInvalid2(number: usize) bool {
+fn isInvalid(comptime at_least_two: bool, number: usize) bool {
     const l = std.math.log10_int(number);
 
     for (1..l + 1) |i| {
@@ -105,10 +84,14 @@ fn isInvalid2(number: usize) bool {
         if (remainder != 0 and std.math.log10_int(remainder) == i - 1) {
             var reproduced = remainder;
 
-            var j = i;
-            while (j < l + 1) : (j += i) {
-                const factor = std.math.pow(usize, 10, j);
-                reproduced += remainder * factor;
+            if (at_least_two) {
+                var j = i;
+                while (j < l + 1) : (j += i) {
+                    const factor = std.math.pow(usize, 10, j);
+                    reproduced += remainder * factor;
+                }
+            } else {
+                reproduced += remainder * denominator;
             }
 
             if (reproduced == number) {
@@ -121,27 +104,27 @@ fn isInvalid2(number: usize) bool {
 }
 
 test "isInvalid 101" {
-    try std.testing.expect(!isInvalid(101));
+    try std.testing.expect(!isInvalid(false, 101));
 }
 
 test "isInvalid simple" {
-    try std.testing.expect(isInvalid(22));
+    try std.testing.expect(isInvalid(false, 22));
 }
 
 test "isInvalid complex" {
-    try std.testing.expect(isInvalid(123123));
+    try std.testing.expect(isInvalid(false, 123123));
 }
 
 test "not isInvalid three times" {
-    try std.testing.expect(!isInvalid(222));
+    try std.testing.expect(!isInvalid(false, 222));
 }
 
 test "not isInvalid" {
-    try std.testing.expect(!isInvalid(1223));
+    try std.testing.expect(!isInvalid(false, 1223));
 }
 
 test "not isInvalid long" {
-    try std.testing.expect(!isInvalid(1234512344));
+    try std.testing.expect(!isInvalid(false, 1234512344));
 }
 
 test countInvalid {
@@ -159,5 +142,23 @@ test countInvalid {
         Range{ .from = 2121212118, .to = 2121212124 },
     };
 
-    try std.testing.expectEqual(1227775554, countInvalid(&ranges));
+    try std.testing.expectEqual(1227775554, countInvalid(false, &ranges));
+}
+
+test "countInvalidAtLeastTwo" {
+    const ranges = [_]Range{
+        Range{ .from = 11, .to = 22 },
+        Range{ .from = 95, .to = 115 },
+        Range{ .from = 998, .to = 1012 },
+        Range{ .from = 1188511880, .to = 1188511890 },
+        Range{ .from = 222220, .to = 222224 },
+        Range{ .from = 1698522, .to = 1698528 },
+        Range{ .from = 446443, .to = 446449 },
+        Range{ .from = 38593856, .to = 38593862 },
+        Range{ .from = 565653, .to = 565659 },
+        Range{ .from = 824824821, .to = 824824827 },
+        Range{ .from = 2121212118, .to = 2121212124 },
+    };
+
+    try std.testing.expectEqual(4174379265, countInvalid(true, &ranges));
 }
